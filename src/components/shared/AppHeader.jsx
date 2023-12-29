43d225dfa3d8e09c8d13744556d378c280a2386a
Albert Urbina <johan50693@gmail.com>
2023-12-29 01:11:55 -0400

    Se eliminan elementos sin usar

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -3,10 +3,7 @@ import { FiMenu, FiMoon, FiSun, FiX } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import useThemeSwitcher from '../../hooks/useThemeSwitcher';
 import HireMeModal from '../HireMeModal';
-import logoLight from '../../images/logo-light.svg';
-import logoDark from '../../images/logo-dark.svg';
 import { motion } from 'framer-motion';
-import Button from '../reusable/Button';
 
 const AppHeader = () => {
 	const [showMenu, setShowMenu] = useState(false);
