ab2802950c9c9307d93f72a1d22a3eec527b57d2
Albert Urbina <johan50693@gmail.com>
2024-01-06 22:26:49 -0400

    Se oculta informacion de contacto

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -108,13 +108,13 @@ const AppHeader = () => {
 					>
 						Acerca de mi
 					</Link> */}
-					<Link
+					{/* <Link
 						to="/contact"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 						aria-label="Contacto"
 					>
 						Contacto
-					</Link>
+					</Link> */}
 				</div>
 
 				{/* Header links large screen */}
@@ -140,13 +140,13 @@ const AppHeader = () => {
 					>
 						Acerca de mi
 					</Link> */}
-					<Link
+					{/* <Link
 						to="/contact"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
 						aria-label="Contacto"
 					>
 						Contacto
-					</Link>
+					</Link> */}
 				</div>
 
 				{/* Header right section buttons */}
